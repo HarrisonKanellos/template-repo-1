592d0e384693ff053a00e8352a1a1076939e446b
HarrisonKanellos <harrisonkanellos@gmail.com>
2026-07-16 14:49:47 +1000

    Add jest and browser globals in eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,16 @@
 // eslint.config.js
 const { defineConfig } = require("eslint/config");
+import globals from "globals";
 
 module.exports = defineConfig([
     { 
         files: [["src/*", "**/.js"]], 
         languageOptions: { 
-            globals: globals.browser
-        } 
+            globals: {
+                ...globals.browser,
+                ...globals.jest,
+            },  
+        } ,
     },
 	{ 
         files: [["src/*", "**/.js"]], 
